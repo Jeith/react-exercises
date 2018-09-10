070b5fcbc19b1668cc033667f10630c94c054a1a
Jeith <keithp1208@yahoo.com>
2018-09-10 13:49:41 -0500

    finished stylizing of the memory card game

---
+++ memorygame/memory-game/src/MemoryCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './MemoryCard.css';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="MemoryCard">
+            <img src={"https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png"}/>
+            </div>
+        )
+    }
+}
+
+export default App;
