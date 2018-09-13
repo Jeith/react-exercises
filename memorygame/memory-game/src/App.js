966de1a426f22a686584004ce14614b131374033
Jeith <keithp1208@yahoo.com>
2018-09-12 20:49:11 -0500

    halfway through part 3 of memory game

--- memorygame/memory-game/src/App.js
+++ memorygame/memory-game/src/App.js
@@ -2,8 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 import MemoryCard from './MemoryCard.js';
 
+function generateDeck(){
+  var symbols = ['∆', 'ß', '£', '§', '•', '$', '+', 'ø'];
+  var deck = [];
+  for (var i = 0; i < 16; i++){
+    deck.push({isFlipped: false, symbol: symbols[i%8]})
+  }
+  shuffle(deck)
+  return deck;
+}
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deck: generateDeck(),
+      pickedCards: []
+    }
+	}
   render() {
+    var cardsJSX = this.state.deck.map((card, index) => {
+			return <MemoryCard />
+		});
+		console.log(cardsJSX)
     return (
       <div className="App">
         <header className="App-header">
@@ -11,28 +42,16 @@ class App extends Component {
           <h4>Match cards to win</h4>
         </header>
       <div>
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
+      {cardsJSX.slice(0,4)}
       </div>
       <div>
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
+      {cardsJSX.slice(4,8)}
       </div>
       <div>
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
+      {cardsJSX.slice(8,12)}
       </div>
       <div>
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
-        <MemoryCard/>
+      {cardsJSX.slice(12,16)}
       </div>
       </div>
     );
--- memorygame/memory-game/src/MemoryCard.js
+++ memorygame/memory-game/src/MemoryCard.js
@@ -3,13 +3,33 @@ import './MemoryCard.css';
 import './App.css';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="MemoryCard">
-                <img src={"https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png"}/>
-            </div>
-        )
-    }
+
+	constructor() {
+		super();
+		this.state = {isFlipped: false};
+	}
+	clickHandler() {
+		this.setState({
+			isFlipped: !this.state.isFlipped
+		})
+	}
+
+	render() {
+		var cardClass = "MemoryCardInner";
+		if (this.state.isFlipped) {
+		    cardClass += " flipped";
+		};
+		return (
+		    <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+		        <div className={cardClass}>
+		            <div className="MemoryCardBack">
+		                <img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt="" />
+		            </div>
+		            <div className="MemoryCardFront">∆</div>
+		        </div>
+		    </div>
+		);
+	}
 }
 
 export default App;
